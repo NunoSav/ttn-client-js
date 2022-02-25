1867bc4c2298d6ccd34a0e994a089d4268b24190
Nuno Rodrigues <nrodrigues@neoception.com>
2022-02-25 11:13:25 +0000

    fix: define and use proper payload structure of a downlink

--- src/downlink.ts
+++ src/downlink.ts
@@ -1,45 +1,60 @@
-import axios from "axios";
-import { ttnConfig } from "./client";
-import { Device } from "./device";
+import axios from 'axios';
+import { ttnConfig } from './client';
+import { Device } from './device';
 
 //#region Interfaces
+export interface Payload {
+  downlinks: Downlink[];
+}
+
+export interface Downlink {
+  confirmed: boolean;
+  frm_payload: string;
+  f_port: number;
+}
+
 enum DownlinkQueueMethod {
-  Push = "push",
-  Replace = "replace",
+  Push = 'push',
+  Replace = 'replace',
 }
 //#endregion
 
 //#region Methods
 export async function downlinkQueuePush(
   devices: Device[],
-  base64Payload: string
+  payload: Payload
 ): Promise<void> {
+  if (payload.downlinks.length === 0) throw new Error('Payload contains no downlink data!');
+
   for await (const device of devices) {
-    await queueDownlink(device, base64Payload, DownlinkQueueMethod.Push);
+    await queueDownlink(device, payload, DownlinkQueueMethod.Push);
   }
 }
 
 export async function downlinkQueueReplace(
   devices: Device[],
-  base64Payload: string
+  payload: Payload
 ): Promise<void> {
+  if (payload.downlinks.length === 0) throw new Error('Payload contains no downlink data!');
+
   for await (const device of devices) {
-    await queueDownlink(device, base64Payload, DownlinkQueueMethod.Replace);
+    await queueDownlink(device, payload, DownlinkQueueMethod.Replace);
   }
 }
 
 export async function queueDownlink(
   device: Device,
-  base64Payload: string,
+  payload: Payload,
   method: DownlinkQueueMethod
 ): Promise<void> {
   if (!device.ids.application_ids.application_id)
-    throw new Error("Device lacks application identifier!");
+    throw new Error('Device lacks application identifier!');
 
   const appId = device.ids.application_ids.application_id;
   const devId = device.ids.device_id;
   const url = `${ttnConfig.domain}/api/v3/as/applications/${appId}/devices/${devId}/down/${method}`;
+  const data = JSON.stringify(payload);
 
-  await axios.post(url, base64Payload, { headers: ttnConfig.headers });
+  await axios.post(url, data, { headers: ttnConfig.headers });
 }
 //#endregion
